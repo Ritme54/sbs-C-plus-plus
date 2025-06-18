4e82491f35da616bcebdbef2306e8048518a3d58
Ritme54 <tmdwns5456@naver.com>
2025-06-18 15:43:42 +0900

    2025.06.18(C++마무리단계)

---
+++ program/Class7th(Template)/Class7th(Template).cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <cstring> 
+
+#define SIZE 5
+
+using namespace std;
+
+template <typename T>
+bool Same(T left, T right)
+{
+	return left == right;
+}
+
+template <>
+bool Same(const char* left, const char* right)
+{
+	return strlen(left) == strlen(right);
+}
+
+
+
+template<typename T>
+class Container
+{
+private:
+	int Index;
+	T list[SIZE];
+
+public:
+	Container()
+	{
+		Index = 0;
+		for (int i = 0; i < SIZE; i++)
+		{
+			list[i] = NULL;
+		}
+	}
+
+	void Add(T data)
+	{
+
+		if (Index < SIZE)
+		{
+			list[Index++] = data;
+		}
+		else
+		{
+			cout << "Index Out of Range" << endl;
+		}
+	}
+
+	const T& operator[](int count)
+	{
+		return list[count];
+	}
+
+};
+
+
+
+
+int main()
+{
+#pragma region 템플릿
+	// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터 형식을 가질 수 있는
+	// 기술에 중점을 두어 재사용성을 높일 수 있는 기능.
+
+
+	//Same(5, 5);
+	//
+	//cout << "(5, 5)의 값 : "<< Same<int>(5, 5) << endl;
+	//cout << "(5, 6)의 값 : " << Same<int>(5, 6) << endl;
+	//cout << "('A', 'A')의 값 : " << Same<char>('A', 'A') << endl;
+	//cout << "(5, 6)의 값 : " << Same<int>(5, 6) << endl;
+
+#pragma endregion
+
+#pragma region 템플릿 특수화
+	//특정 자료형에 대해 다르게 처리하고 싶은 경우
+	//특정한 자료형만 다른 형식으로 동작시키는 경우
+	//template<(비워둔다)>
+	//bool Same(const char* left, const char* right) 
+
+	//cout << Same("ABC","DEF") << endl;
+#pragma endregion
+
+#pragma region MyRegion
+
+		//Container<int> container;
+		//container.Add(10);
+		//container.Add(20);
+		//container.Add(30);
+		//container.Add(40);
+		//container.Add(50);
+
+
+
+#pragma endregion
+
+
+
+
+
+
+}
--- program/program/program.cpp
+++ program/program/program.cpp
@@ -1,105 +1,160 @@
 ﻿#include <iostream>
-#include <cstring> 
-
-#define SIZE 5
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
-template <typename T>
-bool Same(T left, T right)
+
+int main()
 {
-	return left == right;
-}
-
-template <>
-bool Same(const char* left, const char* right)
-{
-	return strlen(left)== strlen(right);
-}
-
-
-
-template<typename T>
-class Container
-{
-private:
-	int Index;
-	T list[SIZE];
-
-public:
-	Container()
-	{
-		Index = 0;
-		for (int i = 0; i < SIZE; i++)
-		{
-			list[i] = NULL;
-		}
-	}
-
-	void Add(T data)
-	{
-
-		if (Index < SIZE)
-		{
-			list[Index++] = data;
-		}
-		else
-		{
-			cout << "Index Out of Range" << endl;
-		}
-	}
-
-	const T& operator[](int count)
-	{
-		return list[count];
-	}
-
-};
-
-
-
-
-	int main()
-	{
-#pragma region 템플릿
-		// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터 형식을 가질 수 있는
-		// 기술에 중점을 두어 재사용성을 높일 수 있는 기능.
-
-
-		//Same(5, 5);
-		//
-		//cout << "(5, 5)의 값 : "<< Same<int>(5, 5) << endl;
-		//cout << "(5, 6)의 값 : " << Same<int>(5, 6) << endl;
-		//cout << "('A', 'A')의 값 : " << Same<char>('A', 'A') << endl;
-		//cout << "(5, 6)의 값 : " << Same<int>(5, 6) << endl;
-
-#pragma endregion
-
-#pragma region 템플릿 특수화
-	//특정 자료형에 대해 다르게 처리하고 싶은 경우
-	//특정한 자료형만 다른 형식으로 동작시키는 경우
-	//template<(비워둔다)>
-	//bool Same(const char* left, const char* right) 
-
-	//cout << Same("ABC","DEF") << endl;
-#pragma endregion
-
-#pragma region MyRegion
-
-		Container<int> container;
-		container.Add(10);
-		container.Add(20);
-		container.Add(30);
-		container.Add(40);
-		container.Add(50);
-
-
-
-#pragma endregion
-
-
-
+#pragma region 선형 컨테이너(STL)
 	
+#pragma region vector container
 	
+	//vector<int> vector;
+	//
+	//vector.reserve(10); //백터의 통 크기를 미리 정하고 시작한다.
+	////메모리 파편화 등을 방지할 수 있다.
+	//
+	//vector.push_back(10);
+	//vector.push_back(20);
+	//vector.push_back(30);
+	//vector.push_back(40);
+	//vector.push_back(50);
+	//
+	//vector.pop_back();
+	//vector.pop_back();
+	//vector.pop_back();//데이터 값만 제거하는 식
+	//
+	//
+	//
+	//cout << vector.size() << endl;
+	//
+	//for (int i = 0; i < vector.size(); i++)
+	//{
+	//	cout << vector[i] << "  ";
+	//}
+	//
+	////백터의 내용물 양의 크기
+	//
+	//cout << "vector capacity : " << vector.capacity() << "  ";
+	////현재 백터의 총 크기(통의 크기)
+	////capacity : 메모리의 현재 크기
 
+
+#pragma endregion
+
+#pragma region list container
+	
+	//list<int> list;
+	//
+	//list.push_back(20);
+	//
+	//for (int value : list) {
+	//	std::cout << value << " ";
+	//}
+	//std::cout << std::endl;
+	//
+	//list.push_back(30);
+	//
+	//for (int value : list) {
+	//	std::cout << value << " ";
+	//}
+	//std::cout << std::endl;
+	//
+	//list.push_front(10);
+	//
+	//for (int value : list) {
+	//	std::cout << value << " ";
+	//}
+	//std::cout << std::endl;
+	//
+	//list.pop_front();
+	//for (int value : list) {
+	//	std::cout << value << " ";
+	//}
+
+#pragma endregion
+
+#pragma region string
+	//문자열 클래스
+
+	//string content;
+	//
+	//cout << content.capacity() << endl;
+	//
+	//content = "League of Legend";
+	//
+	//cout << content << endl; //Heap에 메모리를 올림
+	//cout << content.capacity() << endl;
+	//
+	//	content = "Battelfild";
+	//
+	//	cout << content << endl; // 
+	//	cout << content.capacity() << endl;
+	//	
+	//	content.append("1");
+	//
+	//	cout << content << endl; // 
+#pragma endregion
+
+#pragma region 컨테이너 어댑터
+
+	//컨테이너들에서 특정 기능만 사용할 수 있도록 추출한(?)기능
+
+#pragma region stack 컨테이너
+
+	//마지막에 넣은 데이터가 가장 처음 나가는 형태.
+	// (프링글스또는 카드 뽑기)
+	//stack<int>stack;
+	//
+	//stack.push(10);
+	//stack.push(20);
+	//stack.push(30);
+	//stack.push(40);
+	//while (stack.empty() == false)
+	//{
+	//	cout << stack.top() << endl;
+	//	stack.pop();
+	//}
+	//cout << stack.size() << endl;
+	
+#pragma endregion
+
+#pragma region queue 컨테이너
+	// 선입선출.
+
+//queue<int>queue;
+//
+//queue.push(10);
+//queue.push(20);
+//queue.push(30);
+//queue.push(40);
+//queue.push(50);
+//
+//cout << queue.size() << endl;
+//
+//while (queue.empty() == false)
+//{
+//	cout << queue.front() << endl;
+//	queue.pop();
+//}
+
+#pragma endregion
+
+
+#pragma endregion
+
+
+#pragma endregion
+
+
+
+
+
+	return 0;
 }
+
+
